f85573ab0238b4f79ce90759c3179f4c160fcebe
Tijana Milicevic <tijana@MacBook-od-Korisnik.local>
2018-11-23 23:57:44 +0100

    first commit

---
+++ WindowShopper/WindowShopper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WindowShopper
+//
+//  Created by Tijana Milicevic on 11/23/18.
+//  Copyright © 2018 TT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
